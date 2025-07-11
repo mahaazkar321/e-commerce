9cc56508d15fc0bddb08a461c2b34b225cf72d9c
mahaazkar321 <mahaazkar@gmail.com>
2025-07-11 15:43:20 +0500

    comiting

--- ecomm/src/main.jsx
+++ ecomm/src/main.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // ✅ import BrowserRouter
-import App from './App.jsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 import './assets/css/style.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter> {/* ✅ wrap App with BrowserRouter */}
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>
-);
+)
