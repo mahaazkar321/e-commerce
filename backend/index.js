bb44184c0f6e4d3a84fd0d6adaf1c478ada3d7e2
eemankhalid <eemankhalid04@gmail.com>
2025-07-17 14:06:18 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,15 @@
 const connectToMongo=require('./db')
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 connectToMongo();
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
