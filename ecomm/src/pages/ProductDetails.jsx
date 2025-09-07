1ebb29e5292376c16fa019f98c2782b72197edc5
Tashfeen Khan <141846927+tashfeen-khann@users.noreply.github.com>
2025-09-07 13:40:48 +0500

    fixings

--- ecomm/src/pages/ProductDetails.jsx
+++ ecomm/src/pages/ProductDetails.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
-import Details from '../components/DetailsProduct/Details';
+import Details from "../components/DetailsProduct/Details";
 import RelatedItems from '../components/DetailsProduct/RelatedItems';
 
 const ProductDetails = () => {
